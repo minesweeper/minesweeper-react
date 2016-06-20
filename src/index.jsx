85ce7eb880f1a5100408fdb24dcb11eca0cbcd6a
Mark Ryall <mark@ryall.name>
2016-06-21 00:03:53 +1000

    just replace the entire document body and remove div for game

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import {render} from 'react-dom';
 import Game from './components/Game';
 
-render(<Game />, document.getElementById('game'));
+render(<Game />, document.body);
