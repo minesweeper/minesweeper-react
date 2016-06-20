5faeb77a658c2808ebac952ebdb95dab7d1a56dc
Mark Ryall <mark@ryall.name>
2016-06-20 22:17:13 +1000

    add the minesweeper title

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,13 @@
-console.log('here we go');
+import React from 'react';
+import {render} from 'react-dom';
+
+const element = document.getElementById('game');
+
+render(
+  <div className="minesweeper">
+    <div className="title">
+      <span className="title-left"></span>
+      <span className="title-right"></span>
+    </div>
+  </div>, element
+);
