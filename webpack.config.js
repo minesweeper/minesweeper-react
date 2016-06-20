28cbba494ac8186e5d9acd41f744018ebc08dc1d
Mark Ryall <mark@ryall.name>
2016-06-20 21:58:39 +1000

    sprinkle of webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var WebpackErrorNotificationPlugin = require('webpack-error-notification');
+
+module.exports = {
+  entry: __dirname + "/src/index.jsx",
+  output: {
+    path: __dirname,
+    filename: "scripts.js"
+  },
+
+  resolve: {
+    modulesDirectories: ["node_modules"],
+    extensions: ['', '.js', '.jsx']
+  },
+
+  plugins: [
+    new WebpackErrorNotificationPlugin()
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
