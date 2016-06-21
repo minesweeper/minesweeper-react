d39609a55340bc7977f701f25b08d5cddb580a52
Mark Ryall <mark@ryall.name>
2016-06-21 20:43:14 +1000

    cleaned up ugly implementation of Lcd and added spec

---
+++ src/components/Game/Outer/Top/Lcd/index-spec.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Digit from './Digit';
+import Lcd from './index.jsx';
+
+describe('Lcd', () => {
+  it('should render default number of 000', () => {
+    const tree = sd.shallowRender(<Lcd className="theClassName" />);
+    expect(tree.getRenderOutput()).toEqualJSX(
+      <div className='theClassName'>
+        <Digit digit='0' />
+        <Digit digit='0' />
+        <Digit digit='0' />
+      </div>
+    );
+  });
+
+  it('should render each digit of 3 digit number', () => {
+    const tree = sd.shallowRender(<Lcd className="theClassName" number='321' />);
+    expect(tree.getRenderOutput()).toEqualJSX(
+      <div className='theClassName'>
+        <Digit digit='3' />
+        <Digit digit='2' />
+        <Digit digit='1' />
+      </div>
+    );
+  });
+
+  it('should drop any extra digits', () => {
+    const tree = sd.shallowRender(<Lcd className="theClassName" number='54321' />);
+    expect(tree.getRenderOutput()).toEqualJSX(
+      <div className='theClassName'>
+        <Digit digit='3' />
+        <Digit digit='2' />
+        <Digit digit='1' />
+      </div>
+    );
+  });
+});
--- src/components/Game/Outer/Top/Lcd/index.jsx
+++ src/components/Game/Outer/Top/Lcd/index.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import Digit from './Digit';
+import _ from 'lodash';
 
 const Lcd = (props) => {
   const number_of_digits = 3;
-  const first = 0;
-  const second = 1;
-  const third = 2;
-  const digits = (`000${props.number}`).slice(-number_of_digits);
-
-  return (
-    <div className={props.className}>
-      <Digit digit={digits[first]} />
-      <Digit digit={digits[second]} />
-      <Digit digit={digits[third]} />
-    </div>
-  );
+  const default_number = 0;
+  const number = props.number || default_number;
+  const digits = (`${_.repeat('0', number_of_digits)}${number}`).slice(-number_of_digits);
+  const children = _.map(_.range(number_of_digits), (index) => <Digit key={index} digit={digits[index]} />);
+  return <div className={props.className}>{children}</div>;
 };
 
 export default Lcd;
