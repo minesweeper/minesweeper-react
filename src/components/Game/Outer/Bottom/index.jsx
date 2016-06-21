031050b59257421806a2c87e78b1481d6a7bdc8e
Mark Ryall <mark@ryall.name>
2016-06-21 21:47:33 +1000

    add individual cells to board (unclicked)

--- src/components/Game/Outer/Bottom/index.jsx
+++ src/components/Game/Outer/Bottom/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import styles from './styles.css';
+import _ from 'lodash';
 
-const Bottom = () => (
-  <div className={styles.bottom}>
-  </div>
-);
+const Bottom = () => {
+  const number_of_rows = 16;
+  const number_of_cols = 30;
+
+  const rows = _.map(_.range(number_of_rows), (row) => {
+    const cols = _.map(_.range(number_of_cols), (col) => {
+      return <td key={`${row}.${col}`} className={`${styles.unclicked} ${styles.field}`}></td>;
+    });
+    return <tr key={row}>{cols}</tr>;
+  });
+  return(
+    <div className={styles.bottom}>
+      <table>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Bottom;
