e8b6f6da4dd849c7ed3c76971371fe360593f4ef
Mark Ryall <mark@ryall.name>
2016-06-23 00:55:55 +1000

    add game dimensions

--- src/minesweeper/index-spec.js
+++ src/minesweeper/index-spec.js
@@ -1,8 +1,13 @@
 import minesweeper from './index.js';
 
+const default_row_count = 16;
+const default_column_count = 30;
+
 describe('minesweeper', () => {
-  it('should have initial state', () => {
+  it('should have initial state for default game', () => {
     const game = minesweeper();
+    assert.equal(default_row_count, game.row_count);
+    assert.equal(default_column_count, game.column_count);
     assert.equal(false, game.finished());
   });
 });
--- src/minesweeper/index.js
+++ src/minesweeper/index.js
@@ -1,6 +1,14 @@
-const minesweeper = () => {
+const default_row_count = 16;
+const default_column_count = 30;
+
+const minesweeper = (options) => {
+  const configuration = options || {};
+  const row_count = configuration.row_count || default_row_count;
+  const column_count = configuration.column_count || default_column_count;
   const finished = () => false;
   return {
+    row_count: row_count,
+    column_count: column_count,
     finished: finished
   };
 };
