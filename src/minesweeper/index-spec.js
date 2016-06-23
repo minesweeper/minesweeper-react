de5047e6ac6b17eea011a086c1166c3886a5a2a7
Mark Ryall <mark@ryall.name>
2016-06-23 21:25:48 +1000

    spec for mine sweeper game with specified size

--- src/minesweeper/index-spec.js
+++ src/minesweeper/index-spec.js
@@ -10,4 +10,13 @@ describe('minesweeper', () => {
     assert.equal(default_column_count, game.column_count);
     assert.equal(false, game.finished());
   });
+
+  it('should have initial state for configured game', () => {
+    const row_count = 1;
+    const column_count = 2;
+    const game = minesweeper({row_count: row_count, column_count: column_count});
+    assert.equal(row_count, game.row_count);
+    assert.equal(column_count, game.column_count);
+    assert.equal(false, game.finished());
+  });
 });
