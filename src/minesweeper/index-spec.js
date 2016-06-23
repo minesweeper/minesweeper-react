81c4b16eb1b83e940e83756e1c07855eb978aa99
Mark Ryall <mark@ryall.name>
2016-06-23 22:45:41 +1000

    add mine count parameter to game

--- src/minesweeper/index-spec.js
+++ src/minesweeper/index-spec.js
@@ -2,21 +2,29 @@ import minesweeper from './index.js';
 
 const default_row_count = 16;
 const default_column_count = 30;
+const default_mine_count = 99;
 
 describe('minesweeper', () => {
   it('should have initial state for default game', () => {
     const game = minesweeper();
     assert.equal(default_row_count, game.row_count);
     assert.equal(default_column_count, game.column_count);
+    assert.equal(default_mine_count, game.mine_count);
     assert.equal(false, game.finished());
   });
 
   it('should have initial state for configured game', () => {
     const row_count = 1;
     const column_count = 2;
-    const game = minesweeper({row_count: row_count, column_count: column_count});
+    const mine_count = 1;
+    const game = minesweeper({
+      row_count: row_count,
+      column_count: column_count,
+      mine_count: mine_count
+    });
     assert.equal(row_count, game.row_count);
     assert.equal(column_count, game.column_count);
+    assert.equal(mine_count, game.mine_count);
     assert.equal(false, game.finished());
   });
 });
