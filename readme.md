8e5de021797c878714b762be2c041c8a4a7b061a
Mark Ryall <mark@ryall.name>
2016-06-28 20:42:13 +1000

    make cells subscribe to notifications from game and update to correct style

--- readme.md
+++ readme.md
@@ -4,3 +4,11 @@
 npm install
 ./bin/watch
 ```
+
+## interacting with the game from js console
+
+```javascript
+g = games[0];
+g.reveal([1, 2]);
+g.mark([2, 3])
+```
--- src/components/Game/Outer/Bottom/Cell/index.jsx
+++ src/components/Game/Outer/Bottom/Cell/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import styles from './styles.css';
+import styleForCellState from './styleForCellState';
+import {isEqual} from 'lodash';
 
 class Cell extends React.Component {
   constructor(props) {
     super(props);
+    props.game.onCellStateChange(
+      (cell, state) => {
+        if (isEqual(cell, props.position)) {
+          const style = styles[styleForCellState(state)];
+          this.setState({style: style});
+        }
+      }
+    );
+    this.state = { style: styles.unknown };
   }
 
   render() {
-    const className = `${styles.field} ${styles.unknown}`;
+    const className = `${styles.field} ${this.state.style}`;
     return <td className={className} />;
   }
 }
