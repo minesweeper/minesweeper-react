ac6da654f93bd32c86d66a216d7a48290f668fa6
Mark Ryall <mark@ryall.name>
2016-06-28 21:04:15 +1000

    try to simulate long tap with click last longer than 500 ms

--- src/components/Game/Outer/Bottom/Cell/index.jsx
+++ src/components/Game/Outer/Bottom/Cell/index.jsx
@@ -3,6 +3,8 @@ import styles from './styles.css';
 import styleForCellState from './styleForCellState';
 import {isEqual} from 'lodash';
 
+const now = () => (new Date()).getTime()
+
 class Cell extends React.Component {
   constructor(props) {
     super(props);
@@ -14,16 +16,25 @@ class Cell extends React.Component {
         }
       }
     );
-    this.onClick = (event) => {
+    this.onMouseDown = (event) => {
       event.preventDefault();
-      props.game.reveal(props.position);
+      this.setState({clickStart: now()})
+    };
+    this.onMouseUp = (event) => {
+      event.preventDefault();
+      const duration = now() - this.state.clickStart;
+      if (duration < 500) {
+        props.game.reveal(props.position);
+      } else {
+        props.game.mark(props.position);
+      }
     };
     this.state = { style: styles.unknown };
   }
 
   render() {
     const className = `${styles.field} ${this.state.style}`;
-    return <td onClick={this.onClick} className={className} />;
+    return <td onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp} className={className} />;
   }
 }
 
