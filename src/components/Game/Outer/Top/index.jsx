d1e34cb4a101f68c280b162c939cd5f4f39952ce
Mark Ryall <mark@ryall.name>
2016-07-10 19:46:40 +1000

    attach lcd to mine count and timer

--- src/components/Game/Outer/Top/index.jsx
+++ src/components/Game/Outer/Top/index.jsx
@@ -3,12 +3,37 @@ import Lcd from './Lcd';
 import Status from './Status';
 import styles from './styles.css';
 
-const Top = () => (
-  <div className={styles.top}>
-    <Lcd number={99} className={styles.minesRemaining} />
-    <Status className={styles.status} />
-    <Lcd number={0} className={styles.timer} />
-  </div>
-);
+class Top extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    const game = context.game;
+    context.game.onRemainingMineCountChange(
+      (count) => { this.setState({mineCount: count}); }
+    );
+    context.game.onTimerChange(
+      (ms) => { this.setState({seconds: Math.floor(ms/1000)}); }
+    );
+
+    this.state = {
+      mineCount: game.remainingMineCount(),
+      seconds: 0
+    };
+  }
+
+  render() {
+    const state = this.state;
+    return (
+      <div className={styles.top}>
+        <Lcd number={state.mineCount} event='RemainingMineCount' className={styles.minesRemaining} />
+        <Status className={styles.status} />
+        <Lcd number={state.seconds} event='Timer' className={styles.timer} />
+      </div>
+    );
+  }
+}
+
+Top.contextTypes = {
+  game: React.PropTypes.object
+};
 
 export default Top;
