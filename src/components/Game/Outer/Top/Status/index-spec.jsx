54bc9335bcb895e86d62a7c1fd500a77458d7a80
Alister Scott <alister.scott@automattic.com>
2016-07-20 16:43:50 +1000

    Split the tests for Status into different tests

--- src/components/Game/Outer/Top/Status/index-spec.jsx
+++ src/components/Game/Outer/Top/Status/index-spec.jsx
@@ -4,38 +4,51 @@ import {gameStates} from 'mines';
 import styles from './styles.css';
 
 describe('Status', () => {
-  it('should render a child span element with a status', () => {
-    let onGameChange = null;
-    const game = {
-      onGameStateChange: (onGameStateChangeCb) => { onGameChange = onGameStateChangeCb; },
-      state: () => gameStates.NOT_STARTED
-    };
+  let onGameChange = null;
+  let tree = null;
+  let instance = null;
+  const game = {
+    onGameStateChange: (onGameStateChangeCb) => { onGameChange = onGameStateChangeCb; },
+    state: () => gameStates.NOT_STARTED
+  };
+
+  beforeEach('set up component', () => {
     styles.status = 'status';
     styles.alive = 'alive';
     styles.alivePressed = 'alivePressed';
     styles.won = 'won';
     styles.dead = 'dead';
-    const tree = sd.shallowRender(<Status game={game} />);
-    const instance = tree.getMountedInstance();
+    tree = sd.shallowRender(<Status game={game} />);
+    instance = tree.getMountedInstance();
+  });
+
+  it('should render a child span element with a default status of alive', () => {
+
     expect(tree.getRenderOutput()).toEqualJSX(
       <span className='status alive' onMouseDown={instance.onMouseDown} onMouseUp={instance.onMouseUp} onTouchStart={instance.onMouseDown} onTouchEnd={instance.onMouseUp}>
       </span>
     );
+  });
 
+  it('should render a child span element when the game has been won', () => {
     onGameChange(gameStates.WON);
     tree.reRender(<Status game={game} />);
     expect(tree.getRenderOutput()).toEqualJSX(
       <span className='status won' onMouseDown={instance.onMouseDown} onMouseUp={instance.onMouseUp} onTouchStart={instance.onMouseDown} onTouchEnd={instance.onMouseUp}>
       </span>
     );
+  });
 
+  it('should render a child span element when the game has been lost', () => {
     onGameChange(gameStates.LOST);
     tree.reRender(<Status game={game} />);
     expect(tree.getRenderOutput()).toEqualJSX(
       <span className='status dead' onMouseDown={instance.onMouseDown} onMouseUp={instance.onMouseUp} onTouchStart={instance.onMouseDown} onTouchEnd={instance.onMouseUp}>
       </span>
     );
+  });
 
+  it('should render a child span element when the button is pressed', () => {
     instance.onMouseDown({preventDefault: () => {}});
     tree.reRender(<Status game={game} />);
     expect(tree.getRenderOutput()).toEqualJSX(
