bcba09b14e3a5cf2b19f823f02ccf8b8e49c8f69
Mark Ryall <mark@ryall.name>
2016-07-31 21:04:15 +1000

    some consistent styling for the controller

--- src/components/Controller/index.jsx
+++ src/components/Controller/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import styles from './styles.css';
 
 class Controller extends React.Component {
   render() {
     return(
-      <div>
-        <div className="playPause" />
-        <div className="gamesWon" />
-        <div className="gamesLost" />
-        <div className="speed" />
-        CONTROLLING {this.props.name}
+      <div className={styles.outer}>
+        <div className={styles.middle}>
+          <div className={styles.inner}>
+            <div className="playPause" />
+            <div className="gamesWon" />
+            <div className="gamesLost" />
+            <div className="speed" />
+            CONTROLLING {this.props.name}
+          </div>
+        </div>
       </div>
     );
   }
