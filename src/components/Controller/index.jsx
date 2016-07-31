53cbcfec8964d69f6712359f6852dfa268cdcfec
Mark Ryall <mark@ryall.name>
2016-07-31 21:23:45 +1000

    attach controller to the corresponding game state

--- src/components/Controller/index.jsx
+++ src/components/Controller/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import styles from './styles.css';
 
 class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    const game = global.minesweeperGames[props.name];
+    if (game) {
+      game.onGameStateChange((newState) => {
+        console.log(`${props.name} is now in state ${newState}`); // eslint-disable-line no-console
+      });
+    }
+  }
+
   render() {
     return (
       <div className={styles.outer}>
