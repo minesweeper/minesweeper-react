76773d49f7f0ddfad7edd54f0ace71b10f73f6e5
Mark Ryall <mark@ryall.name>
2016-07-31 21:29:31 +1000

    have controller keep track of games won and lost

--- src/components/Controller/index.jsx
+++ src/components/Controller/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './styles.css';
+import Lcd from '../Lcd';
+import {gameStates} from 'mines';
 
 class Controller extends React.Component {
   constructor(props) {
@@ -7,19 +9,29 @@ class Controller extends React.Component {
     const game = global.minesweeperGames[props.name];
     if (game) {
       game.onGameStateChange((newState) => {
-        console.log(`${props.name} is now in state ${newState}`); // eslint-disable-line no-console
+        if (game.state() === gameStates.WON) {
+          this.setState({won: this.state.won + 1});
+        }
+        if (game.state() === gameStates.LOST) {
+          this.setState({lost: this.state.lost + 1});
+        }
       });
     }
+    this.state = {
+      won: 0,
+      lost: 0
+    };
   }
 
   render() {
+    const state = this.state;
     return (
       <div className={styles.outer}>
         <div className={styles.middle}>
           <div className={styles.inner}>
             <div className="playPause" />
-            <div className="gamesWon" />
-            <div className="gamesLost" />
+            <Lcd number={state.won} />
+            <Lcd number={state.lost} />
             <div className="speed" />
             CONTROLLING {this.props.name}
           </div>
