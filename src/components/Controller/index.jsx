fb75affabe0ac7b951506f36d35a807e8ff07ea4
Mark Ryall <mark@ryall.name>
2016-07-31 20:54:35 +1000

    make controller an es6 class and add placeholder divs for each child component

--- src/components/Controller/index.jsx
+++ src/components/Controller/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Controller = (props) => (
-  <div>CONTROLLING {props.name}</div>
-);
+class Controller extends React.Component {
+  render() {
+    return(
+      <div>
+        <div className="playPause" />
+        <div className="gamesWon" />
+        <div className="gamesLost" />
+        <div className="speed" />
+        CONTROLLING {this.props.name}
+      </div>
+    );
+  }
+}
 
 export default Controller;
