2e9abcdc5289ce16cc6b7da74d64168f58265edf
Mark Ryall <mark@ryall.name>
2016-08-01 20:32:30 +1000

    add event handlers to buttons in component (just updating display)

--- src/components/Controller/index.jsx
+++ src/components/Controller/index.jsx
@@ -7,36 +7,54 @@ class Controller extends React.Component {
   constructor(props) {
     super(props);
     const game = global.minesweeperGames[props.name];
+    const incrementWon = () => { this.setState({won: this.state.won + 1}); };
+    const incrementLost = () => { this.setState({lost: this.state.lost + 1}); };
     if (game) {
       game.onGameStateChange((newState) => {
-        if (game.state() === gameStates.WON) {
-          this.setState({won: this.state.won + 1});
-        }
-        if (game.state() === gameStates.LOST) {
-          this.setState({lost: this.state.lost + 1});
-        }
+        if (newState === gameStates.WON) incrementWon();
+        if (newState === gameStates.LOST) incrementLost();
       });
     }
+    this.increaseSpeed = () => {
+      if (this.state.speed < 9) this.setState({speed: this.state.speed + 1});
+    };
+    this.decreaseSpeed = () => {
+      if (this.state.speed > 1) this.setState({speed: this.state.speed - 1});
+    };
+    this.togglePlayPause = () => {
+      const playingOrPaused = this.state.playingOrPaused;
+      if (playingOrPaused === 'play') {
+        this.setState({playingOrPaused: 'pause'});
+      }
+      if (playingOrPaused === 'pause') {
+        this.setState({playingOrPaused: 'play'});
+      }
+    };
     this.state = {
       won: 0,
-      lost: 0
+      lost: 0,
+      playingOrPaused: 'pause',
+      speed: 5
     };
   }
 
   render() {
+    const states = { play: 'pause', pause: 'play' };
     const state = this.state;
+    const nextState = states[state.playingOrPaused];
+
     return (
       <div className={styles.outer}>
         <div className={styles.middle}>
           <div className={styles.inner}>
-            <button className={`${styles.status} ${styles.play}`} />
+            <button onClick={this.togglePlayPause} className={`${styles.status} ${styles[nextState]}`} />
             <span className={`${styles.status} ${styles.won}`} />
             <Lcd number={state.won} />
             <span className={`${styles.status} ${styles.dead}`} />
             <Lcd number={state.lost} />
-            <button className={`${styles.status} ${styles.minus}`} />
-            <Lcd number='5' numberdigits='1' />
-            <button className={`${styles.status} ${styles.plus}`} />
+            <button onClick={this.decreaseSpeed} className={`${styles.status} ${styles.minus}`} />
+            <Lcd number={state.speed} numberdigits='1' />
+            <button onClick={this.increaseSpeed} className={`${styles.status} ${styles.plus}`} />
           </div>
         </div>
       </div>
